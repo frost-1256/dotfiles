e16b0d758c9064558f4786c85e41ba7565a8e37c
spring <harusan@spring-server.com>
2026-03-23 20:54:59 +0900

    Added Keyd and disable copilot key

--- nixos-t14-gen6/configuration.nix
+++ nixos-t14-gen6/configuration.nix
@@ -223,6 +223,19 @@ let
    # Or disable the firewall altogether.
    networking.firewall.enable = true;
    services.fwupd.enable = true;
+   services.keyd = {
+     enable = true;
+       keyboards = {
+         default = {
+           ids = [ "0001:0001:09b4e68d" ];
+             settings = {
+               main = {
+                 "leftshift+leftmeta+f23" = "layer(meta)";
+               };
+             };
+           };
+       };
+   };
 
    # This value determines the NixOS release from which the default
    # settings for stateful data, like file locations and database versions
