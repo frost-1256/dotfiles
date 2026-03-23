9391be5e6827bbf23c823379069cb9947d9db12c
spring <harusan@spring-server.com>
2026-03-23 21:06:13 +0900

    hjkl to 矢印

--- nixos-t14-gen6/configuration.nix
+++ nixos-t14-gen6/configuration.nix
@@ -230,7 +230,13 @@ let
            ids = [ "0001:0001:09b4e68d" ];
              settings = {
                main = {
-                 "leftshift+leftmeta+f23" = "layer(meta)";
+                 "leftshift+leftmeta+f23" = "f13";
+               };
+               "meta" = {
+                 h = "left";
+                 j = "down";
+                 k = "up";
+                 l = "right";
                };
              };
            };
