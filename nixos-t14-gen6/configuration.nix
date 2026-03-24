efe49e5caf011f1a7fe9a71910cc3f57ba515bd2
spring <harusan@spring-server.com>
2026-03-24 20:30:21 +0900

    ロケール関連をmodules/locale.nixに

--- nixos-t14-gen6/configuration.nix
+++ nixos-t14-gen6/configuration.nix
@@ -8,6 +8,7 @@
     [ 
       ./hardware-configuration.nix
       ./modules/boot.nix
+      ./modules/locale.nix
       ./modules/services/keyd.nix
     ];
 
@@ -23,34 +24,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Asia/Tokyo";
-  time.hardwareClockInLocalTime = true;
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocales = ["ja_JP.UTF-8/UTF-8"];
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ja_JP.UTF-8";
-    LC_IDENTIFICATION = "ja_JP.UTF-8";
-    LC_MEASUREMENT = "ja_JP.UTF-8";
-    LC_MONETARY = "ja_JP.UTF-8";
-    LC_NAME = "ja_JP.UTF-8";
-    LC_NUMERIC = "ja_JP.UTF-8";
-    LC_PAPER = "ja_JP.UTF-8";
-    LC_TELEPHONE = "ja_JP.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  i18n.inputMethod = {
-   type = "fcitx5";
-   enable = true;
-   fcitx5.addons = with pkgs; [
-     fcitx5-mozc
-     fcitx5-gtk
-   ];
-  };
-
-  i18n.inputMethod.fcitx5.waylandFrontend = true;
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
